da1468f2c354d4b9bb79cffbdf87b53f9d12b062
iWas-Coder <wasymatieh01@gmail.com>
2024-05-18 22:41:32 +0200

    Basic egui window for the `sk_editor`

--- src/sk_editor.rs
+++ src/sk_editor.rs
@@ -19,6 +19,32 @@
  */
 
 
+extern crate eframe;
+
+mod sk_editor_base;
+
+use eframe::egui;
+
+static SK_EDITOR_NAME: &str = "sparky::editor";
+
 fn main() {
-  println!("Hello, World!");
+  println!("INFO: Initializing {}", SK_EDITOR_NAME);
+  let win_icon = image::open("assets/icon.png").expect("ERROR: Failed to open icon path").to_rgba8();
+  let (win_icon_width, win_icon_height) = win_icon.dimensions();
+  eframe::run_native(
+    SK_EDITOR_NAME,
+    eframe::NativeOptions {
+      viewport: egui::ViewportBuilder::default()
+        .with_inner_size([800.0, 600.0])
+        .with_min_inner_size([800.0, 600.0])
+        .with_icon(egui::IconData {
+          rgba: win_icon.into_raw(),
+          width: win_icon_width,
+          height: win_icon_height
+        }),
+      ..Default::default()
+    },
+    Box::new(|_| Box::new(sk_editor_base::Editor::default()) as Box<dyn eframe::App>)
+  ).expect("Unexpected error. Shutting down...");
+  println!("INFO: {} closed successfully", SK_EDITOR_NAME);
 }
